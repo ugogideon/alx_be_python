73ddebecd2e0c80b1659bdc53706f18dcaa0da66
Adoghe Gideon <ugogideon.35@gmail.com>
2024-09-29 20:43:10 +0000

    add arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -4,6 +4,8 @@ def perform_operation(num1, num2, operation):
         return num1 + num2
     elif operation == 'subtract':
         return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2     
     elif operation == 'divide':
         if num2 == 0:
             return "Error: Division by zero is not allowed."
@@ -11,4 +13,3 @@ def perform_operation(num1, num2, operation):
             return num1 / num2
     else:
         return "Error: Invalid operation."
-                            
